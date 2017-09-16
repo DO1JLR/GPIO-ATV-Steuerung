4e378ce94b08840da38c6f939d391772c82b17e6
L3D <l3d@see-base.de>
2017-09-16 17:11:38 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# https://github.com/DO1JLR/GPIO-ATV-Steuerung
+#
+# Licence: GPL-3.0
+#
+from sys import argv         # fuer die kommandozeilenargumente
+from time import time, sleep # fuer zeit und so...
+try:
+    import RPi.GPIO as GPIO      # raspi gpio-pins
+except:
+    print("\nFehler beim Laden des GPIO Moduls.\n\tLaueft dieses Script auf einem Raspberry Pi 3?\n\tIst Python3 installiert?\n\tSind die GPIOs vorhanden?\n")
+    print("\nWenn du der meinung bist, dass es sich hierbei um\n einen Fehler handelt, dann sag bescheid unter\n'https://github.com/DO1JLR/GPIO-ATV-Steuerung/issues'!\n\tDanke!\n\n")
+    exit()
+#
+# Globale Variabeln:
+#
+version = "0.1"
+debug = False
+
+#
+# Komandozeilenargumente Auslesen
+#
+for i in argv:
+    if i in ["--help", "-h", "/h", "/help", "?", "h"]:
+        print("Moegliche Befehle:\n -h\t--help\tZeige diese Hilfe\n")
+        print(" -v\t--version\tZeigt die Version dieser Software")
+        print("\t--debug\tAktiviere den Debugging Modus")
+        exit()
+    elif i in ["-v", "--version"]:
+        print("Version:\t{0}\n".format(version))
+        exit()
+    elif i == "--debug":
+        debug = True
+        print("\nAktiviere den Debugging-Modus!\n\n")
+debug = True
